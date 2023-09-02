e1d4b396c1ab3767d70bc5b92525bdd2612409d4
panasiuk-r <98800222+panasiuk-r@users.noreply.github.com>
2023-09-02 23:15:22 +0300

    Update Url.ts

--- models/Url.ts
+++ models/Url.ts
@@ -3,20 +3,21 @@ import { DataTypes, Model } from 'sequelize'
 import { sequelize } from '../db/connection'
 
 interface UrlAttributes {
-  shortUrl: string
-  originalUrl: string
+	shortUrl: string
+	originalUrl: string
 }
 
 const Url = sequelize.define<Model<UrlAttributes>>('Url', {
-  shortUrl: {
-    type: DataTypes.STRING,
+	shortUrl: {
+		type: DataTypes.STRING,
 		primaryKey: true,
-    unique: true,
-  }, 
+		unique: true,
+	}, 
+	
 	originalUrl: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  }
+		type: DataTypes.STRING,
+		allowNull: false,
+	}
 })
 
 export { Url, UrlAttributes }
